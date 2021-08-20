491cc0dd07a7932945b589a6b06f0c24cf099df2
Osman Ali YARDIM <7006079@bakircay.edu.tr>
2021-08-20 17:17:29 +0300

    city guide angular first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ValueComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
